28064b7d31e2622675bae2d332c6344fb37a209c
Gabriel <1707gabri@gmail.com>
2018-12-24 18:34:16 +0100

    No Pan and Refactoring

--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,4 @@
-var calculateSunPosition = function(){	
+var calculateSunPosition = function () {
 	var date = new Date();
 	var Year = date.getUTCFullYear();
 	var Month = date.getUTCMonth() + 1;
@@ -6,48 +6,48 @@ var calculateSunPosition = function(){
 	var Hour = date.getUTCHours();
 	var Minute = date.getUTCMinutes();
 	var Second = date.getUTCSeconds();
-  
+
 	console.log(Hour, Minute, Second, Day, Month, Year);
-	
-	var JD = (367*Year) -Math.floor(7.0*(Year+Math.floor((Month+9.0)/12.0))/4.0)+Math.floor(275.0*Month/9.0)+Day+1721013.5+Hour/24.0+Minute/1440.0+Second/86400.0;
+
+	var JD = (367 * Year) - Math.floor(7.0 * (Year + Math.floor((Month + 9.0) / 12.0)) / 4.0) + Math.floor(275.0 * Month / 9.0) + Day + 1721013.5 + Hour / 24.0 + Minute / 1440.0 + Second / 86400.0;
 	d = JD - 2451543.5;
 	var w = 282.9404 + (4.70935 * Math.pow(10, -5) * d);
-    var a = 1.000000;
-    var e = 0.016709 - (1.151 * Math.pow(10, -9) * d);
+	var a = 1.000000;
+	var e = 0.016709 - (1.151 * Math.pow(10, -9) * d);
 	var M = (356.0470 + 0.9856002585 * d) % 360;
 	M >= 0 ? M = M : M = 360 + M;
 	var oblecl = 23.4393 - (3.563 * Math.pow(10, -7) * d);
 	var L = w + M;
-	var E = M + (180/Math.PI) * e * Math.sin(M * Math.PI/180) * (1 + e * Math.cos(M * Math.PI/180));
-	var x = Math.cos(E * Math.PI/180) - e;
-	var y = Math.sin(E * Math.PI/180) * Math.sqrt(1 - e*e);
-	var v = Math.atan2(y, x) * 180/Math.PI;
+	var E = M + (180 / Math.PI) * e * Math.sin(M * Math.PI / 180) * (1 + e * Math.cos(M * Math.PI / 180));
+	var x = Math.cos(E * Math.PI / 180) - e;
+	var y = Math.sin(E * Math.PI / 180) * Math.sqrt(1 - e * e);
+	var v = Math.atan2(y, x) * 180 / Math.PI;
 	var long = v + w;
-	var x = Math.cos(long * Math.PI/180);
-	var y = Math.sin(long * Math.PI/180);
+	var x = Math.cos(long * Math.PI / 180);
+	var y = Math.sin(long * Math.PI / 180);
 	var z = 0;
 	var xE = x;
-	var yE = y*Math.cos(oblecl * Math.PI/180) +z*Math.sin(oblecl * Math.PI/180);
-	var zE = y*Math.sin(oblecl * Math.PI/180) +z*Math.cos(oblecl * Math.PI/180);
-	var RA = (Math.atan2(yE,xE) * 180/Math.PI)%360;   // OK
-	var Decl = Math.atan2(zE,Math.sqrt(xE*xE + yE*yE)) * 180/Math.PI;  // trolig OK
-	var GMST0 = (L+180);
-	var UT = d-Math.floor(d);
-	var SideRealTime = GMST0+UT*360+0;  // ok 
+	var yE = y * Math.cos(oblecl * Math.PI / 180) + z * Math.sin(oblecl * Math.PI / 180);
+	var zE = y * Math.sin(oblecl * Math.PI / 180) + z * Math.cos(oblecl * Math.PI / 180);
+	var RA = (Math.atan2(yE, xE) * 180 / Math.PI) % 360;   // OK
+	var Decl = Math.atan2(zE, Math.sqrt(xE * xE + yE * yE)) * 180 / Math.PI;  // trolig OK
+	var GMST0 = (L + 180);
+	var UT = d - Math.floor(d);
+	var SideRealTime = GMST0 + UT * 360 + 0;  // ok 
 	var HourAngle = SideRealTime - RA;
-	x=Math.cos(HourAngle*Math.PI/180) * Math.cos(Decl*Math.PI/180);
-	y=Math.sin(HourAngle*Math.PI/180) * Math.cos(Decl*Math.PI/180);
-	z=Math.sin(Decl*Math.PI/180);
+	x = Math.cos(HourAngle * Math.PI / 180) * Math.cos(Decl * Math.PI / 180);
+	y = Math.sin(HourAngle * Math.PI / 180) * Math.cos(Decl * Math.PI / 180);
+	z = Math.sin(Decl * Math.PI / 180);
 	// 
-	var EarthLat = Math.atan2(z,Math.sqrt(x*x +y*y)) * 180/Math.PI;
-	var EarthLon = (0*180+RA-GMST0-UT*360) % 360;
+	var EarthLat = Math.atan2(z, Math.sqrt(x * x + y * y)) * 180 / Math.PI;
+	var EarthLon = (0 * 180 + RA - GMST0 - UT * 360) % 360;
 
 
 	//Understandable stuff
 	phi = EarthLat * Math.PI / 180; //° North
 	theta = EarthLon * Math.PI / -180;
 	rho = 5;
-	
+
 	var x = Math.cos(phi) * Math.cos(theta) * rho;
 	var z = Math.cos(phi) * Math.sin(theta) * rho;
 	var y = Math.sin(phi) * rho;
@@ -68,8 +68,8 @@ var calculateSunPosition = function(){
 
 
 
-var loadAndMove = function(){
-	if(!tleAcquired || !finishedLoading)
+var loadAndMove = function () {
+	if (!tleAcquired || !finishedLoading)
 		return;
 
 	var positionEci = satellite.propagate(satrec, new Date()).position;
@@ -79,15 +79,15 @@ var loadAndMove = function(){
 	// console.log(magnitude);
 	var gmst = satellite.gstime(new Date());
 
-	var positionGd    = satellite.eciToGeodetic(positionEci, gmst);
+	var positionGd = satellite.eciToGeodetic(positionEci, gmst);
 	var longitude = positionGd.longitude;
-	var latitude  = positionGd.latitude;
-	var height    = positionGd.height;
-	
+	var latitude = positionGd.latitude;
+	var height = positionGd.height;
+
 	phi = latitude;// * Math.PI / 180; //° North
 	theta = -longitude;// * Math.PI / -180;
 	rho = 2.18;
-	
+
 	var x = Math.cos(phi) * Math.cos(theta) * rho;
 	var z = Math.cos(phi) * Math.sin(theta) * rho;
 	var y = Math.sin(phi) * rho;
@@ -99,12 +99,12 @@ var loadAndMove = function(){
 	moon.rotation.z = latitude;
 	moon.rotation.y = longitude;
 
-	if(track){
+	if (track) {
 		camera.position.x = x * 2;
 		camera.position.z = z * 2;
 		camera.position.y = y * 2;
 	}
-	
+
 	//console.log(myJson.iss_position.latitude * Math.PI / 180,  myJson.iss_position.longitude * Math.PI / -180);
 	console.log(phi, theta)
 	//console.log("P(Long): " + myJson.iss_position.longitude + " : " + phi + "\nT(Lat): " +  myJson.iss_position.latitude + " : " + theta);
@@ -112,28 +112,28 @@ var loadAndMove = function(){
 
 
 
-window.addEventListener( 'resize', onWindowResize, false );
-function onWindowResize(){
-	camera.aspect = (window.innerWidth - 20 )/ (window.innerHeight - 20);
+window.addEventListener('resize', onWindowResize, false);
+function onWindowResize() {
+	camera.aspect = (window.innerWidth - 20) / (window.innerHeight - 20);
 	camera.updateProjectionMatrix();
-	
-    renderer.setSize( window.innerWidth - 20, window.innerHeight - 20);
+
+	renderer.setSize(window.innerWidth - 20, window.innerHeight - 20);
 }
 
 
 var animate = function () {
-	requestAnimationFrame( animate );
-	
-	if(count % 50 == 0 && finishedLoading){
+	requestAnimationFrame(animate);
+
+	if (count % 50 == 0 && finishedLoading) {
 		loadAndMove();
 		sun ? calculateSunPosition() : null;
 		count = 0;
-	}	
+	}
 
 	count += 1;
 	angle += 0.01;
 	controls.update();
-	renderer.render( scene, camera );
+	renderer.render(scene, camera);
 };
 
 
@@ -166,70 +166,70 @@ var sun = false;
 
 // Initialize a satellite record
 var satrec;
-fetch('https://cors.io/?https://spaceflight.nasa.gov/realdata/sightings/SSapplications/Post/JavaSSOP/orbit/ISS/SVPOST.html', {mode: "cors"})
-.then(function(response) {
-	return response.text();	
-})
-.then(function(data) {
-	//console.log(data);
-	data = data.split("TWO LINE MEAN ELEMENT SET")[1];
-	console.log(data.substring(8, 11));
-	console.log(data.substring(16, 85));
-	console.log(data.substring(90, 160));
+fetch('https://cors.io/?https://spaceflight.nasa.gov/realdata/sightings/SSapplications/Post/JavaSSOP/orbit/ISS/SVPOST.html', { mode: "cors" })
+	.then(function (response) {
+		return response.text();
+	})
+	.then(function (data) {
+		//console.log(data);
+		data = data.split("TWO LINE MEAN ELEMENT SET")[1];
+		console.log(data.substring(8, 11));
+		console.log(data.substring(16, 85));
+		console.log(data.substring(90, 160));
 
-	var tleLine1 = data.substring(16, 85);
-	var tleLine2 = data.substring(90, 160);
-	
-	satrec = satellite.twoline2satrec(tleLine1, tleLine2);
-	tleAcquired = true;
-});
+		var tleLine1 = data.substring(16, 85);
+		var tleLine2 = data.substring(90, 160);
+
+		satrec = satellite.twoline2satrec(tleLine1, tleLine2);
+		tleAcquired = true;
+	});
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, (window.innerWidth - 20)/(window.innerHeight - 20), 0.1, 1000 );
-var controls = new THREE.OrbitControls( camera );
+var camera = new THREE.PerspectiveCamera(75, (window.innerWidth - 20) / (window.innerHeight - 20), 0.1, 1000);
+var controls = new THREE.OrbitControls(camera);
 controls.enableKeys = false;
-controls.noPan = true;
-controls.addEventListener( 'start', () => {track = false;} );
+controls.enablePan = false;
+controls.addEventListener('start', () => { track = false; });
 //controls.enableDamping = true;
 
 
 var renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
-renderer.setClearColor( 0xffffff, 0);
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth - 20, window.innerHeight - 20 );
-document.body.appendChild( renderer.domElement );
+renderer.setClearColor(0xffffff, 0);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth - 20, window.innerHeight - 20);
+document.body.appendChild(renderer.domElement);
 
 var lights = [];
-lights[ 0 ] = new THREE.PointLight( 0xffffff, 1.1, 0 );
-lights[ 1 ] = new THREE.PointLight( 0xf0f0f0, 0.1, 0 );
+lights[0] = new THREE.PointLight(0xffffff, 1.1, 0);
+lights[1] = new THREE.PointLight(0xf0f0f0, 0.1, 0);
 
 //Position lights
 calculateSunPosition();
 
-scene.add( lights[ 0 ] );
-scene.add( lights[ 1 ] );
+scene.add(lights[0]);
+scene.add(lights[1]);
 
 
 var geometry = new THREE.SphereGeometry(2, 64, 64);
 //var material = new THREE.MeshPhongMaterial( { color: 0xf5deb3, side: THREE.DoubleSide, flatShading: true } );
-var dayTexture = new THREE.TextureLoader().load( "img/mapday.jpg" );
+var dayTexture = new THREE.TextureLoader().load("img/mapday.jpg");
 dayTexture.wrapS = THREE.RepeatWrapping;
 dayTexture.wrapT = THREE.RepeatWrapping;
-dayTexture.repeat.set( 1, 1 );
+dayTexture.repeat.set(1, 1);
 
-var noDayTexture = new THREE.TextureLoader().load( "img/BlackMarble.jpg" );
+var noDayTexture = new THREE.TextureLoader().load("img/BlackMarble.jpg");
 noDayTexture.wrapS = THREE.RepeatWrapping;
 noDayTexture.wrapT = THREE.RepeatWrapping;
-noDayTexture.repeat.set( 1, 1 );
+noDayTexture.repeat.set(1, 1);
 
 
 var material;
-var dayMaterial = new THREE.MeshLambertMaterial( { map: dayTexture } );
-var noDayMaterial = new THREE.MeshBasicMaterial( { map: noDayTexture } );
+var dayMaterial = new THREE.MeshLambertMaterial({ map: dayTexture });
+var noDayMaterial = new THREE.MeshBasicMaterial({ map: noDayTexture });
 
 sun ? material = dayMaterial : material = noDayMaterial;
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 // var geometry = new THREE.SphereGeometry(0.25, 16, 16);
 // var material = new THREE.MeshPhongMaterial( { color: 0xffff00, side: THREE.DoubleSide, flatShading: true } );
@@ -241,26 +241,26 @@ scene.add( cube );
 
 //3ds files dont store normal maps
 var moon = new THREE.Object3D();
-var loader = new THREE.TDSLoader( );
-loader.load( 'model/isscombined.3ds', function ( object ) {
-	object.traverse( function ( child ) {
-		if ( child instanceof THREE.Mesh ) {
-			if(child.name.substring(0,9) == "polySurfa"){
+var loader = new THREE.TDSLoader();
+loader.load('model/isscombined.3ds', function (object) {
+	object.traverse(function (child) {
+		if (child instanceof THREE.Mesh) {
+			if (child.name.substring(0, 9) == "polySurfa") {
 				child.scale.set(0.005, 0.005, 0.005);
 				console.log(child.name);
-				child.material = new THREE.MeshBasicMaterial( { color: 0xbcbcbc } );
+				child.material = new THREE.MeshBasicMaterial({ color: 0xbcbcbc });
 				moon.add(child.clone());
 			}
-			
-		}
-	} );
 
-	scene.add( moon );
+		}
+	});
+
+	scene.add(moon);
 	finishedLoading = true;
 	loadAndMove();
 	controls.update();
-	renderer.render( scene, camera );
-} );
+	renderer.render(scene, camera);
+});
 // var geometry = new THREE.SphereGeometry(0.05, 16, 16);
 // var material = new THREE.MeshPhongMaterial( { color: 0xffffff, side: THREE.DoubleSide, flatShading: true } );
 // var moon = new THREE.Mesh( geometry, material );
